ea167970387ead92e0cedd5b4f82ac7269d7cbf1
EvilBeaver <ovsiankin.aa@gmail.com>
2016-09-30 16:48:55 +0300

    initial commit

---
+++ OneScript.Mvc/App_Start/ViewConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nustache.Mvc;
+
+namespace OneScript.Mvc
+{
+    public class ViewConfig
+    {
+        public static void RegisterViewEngines(ViewEngineCollection engines)
+        {
+            engines.Insert(0, new NustacheViewEngine
+            {
+                // Comment out this line to require Model in front of all your expressions.
+                // This makes it easier to share templates between the client and server.
+                // But it also means that ViewData/ViewBag is inaccessible.
+                RootContext = NustacheViewEngineRootContext.Model
+            });
+        }
+    }
+}
---
+++ OneScript.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneScript.Mvc.MvcApplication" Language="C#" %>
