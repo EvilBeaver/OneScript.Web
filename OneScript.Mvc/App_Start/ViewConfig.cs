45c666d2537c808122aa979ef68cecb318d11f83
EvilBeaver <ovsiankin.aa@gmail.com>
2017-01-14 12:05:18 +0300

    Рендеринг должен начинаться с основной коллекции ViewData

--- OneScript.Mvc/App_Start/ViewConfig.cs
+++ OneScript.Mvc/App_Start/ViewConfig.cs
@@ -18,7 +18,7 @@ namespace OneScript.Mvc
                 // Comment out this line to require Model in front of all your expressions.
                 // This makes it easier to share templates between the client and server.
                 // But it also means that ViewData/ViewBag is inaccessible.
-                RootContext = NustacheViewEngineRootContext.Model
+                // RootContext = NustacheViewEngineRootContext.Model
             });
 
             ValueGetterFactories.Factories.Clear();
