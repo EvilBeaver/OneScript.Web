3d572c53074a7d2215c8697d9a1c874ab2409dfa
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2017-09-11 20:25:38 +0300

    core 2.0

---
+++ src/OneScript/Infrastructure/OneScriptServicePlugin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneScript.WebHost.Infrastructure
+{
+    public static class OneScriptServicePlugin
+    {
+        public static void AddOneScript(this IServiceCollection services)
+        {
+
+        }
+    }
+}
