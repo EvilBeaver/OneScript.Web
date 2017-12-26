b1b4170c5bd9875c604307582034dfacb9d32470
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2017-12-26 16:04:01 +0300

    Удалил неверное

--- src/OneScript/Startup.cs
+++ src/OneScript/Startup.cs
@@ -28,17 +28,6 @@ namespace OneScript.WebHost
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var conf = new ConfigurationBuilder();
-            conf.AddEnvironmentVariables("OSWEB_");
-            var cfg = conf.Build();
-            services.AddSingleton(cfg);
-
-            var rootDir = cfg.GetValue<string>("APP_CONTENT", null);
-
-            services.Configure<RazorViewEngineOptions>(options =>
-            {
-                options.ViewLocationExpanders.Add(new OscriptViewsOverride(rootDir));
-            });
             services.AddMvc();
             services.AddOneScript();
         }
@@ -46,22 +35,6 @@ namespace OneScript.WebHost
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services, ILogger<Startup> log)
         {
-            if (env.IsDevelopment())
-            {
-                SetContentRoot(env, env.ContentRootPath);
-            }
-            else
-            {
-                var cfg = services.GetService<IConfigurationRoot>();
-                var rootDir = cfg.GetValue<string>("APP_CONTENT", null);
-                log.LogInformation($"Application content: {rootDir?? "null"}");
-                SetContentRoot(env, rootDir ?? Directory.GetCurrentDirectory());
-
-                log.LogInformation($"Content root: {env.ContentRootPath}");
-                log.LogInformation($"Web root: {env.WebRootPath}");
-            }
-
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
