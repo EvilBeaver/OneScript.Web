c3f0bd788abda126bf496f62d3cce6eb606bd345
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2017-12-26 15:55:29 +0300

    Путь к демо-приложению

--- src/OneScript/Infrastructure/OscriptViewsOverride.cs
+++ src/OneScript/Infrastructure/OscriptViewsOverride.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Serialization.Configuration;
 using Microsoft.AspNetCore.Mvc.Razor;
 
 namespace OneScript.WebHost.Infrastructure
 {
     public class OscriptViewsOverride : IViewLocationExpander
     {
-        public void PopulateValues(ViewLocationExpanderContext context)
+        private string _rootPath;
+
+        public OscriptViewsOverride(string rootPath)
         {
+            _rootPath = rootPath;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {    
         }
 
         public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
         {
-            return viewLocations.Select(x => "/resources" + x);
+            var enumer = viewLocations.Select(x => Path.Combine(_rootPath, x.Substring(1)));
+            foreach (var VARIABLE in enumer)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+
+            return enumer;
         }
     }
 }
--- src/OneScript/Startup.cs
+++ src/OneScript/Startup.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OneScript.WebHost.Application;
@@ -12,28 +13,55 @@ namespace OneScript.WebHost
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment hostingEnv, ILoggerFactory logs)
+        public Startup(ILoggerFactory logs)
         {
-            hostingEnv.ContentRootPath = Path.Combine(hostingEnv.ContentRootPath, "resources");
-            hostingEnv.WebRootPath = Path.Combine(hostingEnv.ContentRootPath, "wwwroot");
             logs.AddConsole();
+        }
+
+        private static void SetContentRoot(IHostingEnvironment hostingEnv, string rootDir)
+        {
+            hostingEnv.ContentRootPath = Path.GetFullPath(rootDir);
+            hostingEnv.WebRootPath = Path.Combine(hostingEnv.ContentRootPath, "wwwroot");
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var conf = new ConfigurationBuilder();
+            conf.AddEnvironmentVariables("OSWEB_");
+            var cfg = conf.Build();
+            services.AddSingleton(cfg);
+
+            var rootDir = cfg.GetValue<string>("APP_CONTENT", null);
+
             services.Configure<RazorViewEngineOptions>(options =>
             {
-                options.ViewLocationExpanders.Add(new OscriptViewsOverride());
+                options.ViewLocationExpanders.Add(new OscriptViewsOverride(rootDir));
             });
             services.AddMvc();
             services.AddOneScript();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services, ILogger<Startup> log)
         {
+            if (env.IsDevelopment())
+            {
+                SetContentRoot(env, env.ContentRootPath);
+            }
+            else
+            {
+                var cfg = services.GetService<IConfigurationRoot>();
+                var rootDir = cfg.GetValue<string>("APP_CONTENT", null);
+                log.LogInformation($"Application content: {rootDir?? "null"}");
+                SetContentRoot(env, rootDir ?? Directory.GetCurrentDirectory());
+
+                log.LogInformation($"Content root: {env.ContentRootPath}");
+                log.LogInformation($"Web root: {env.WebRootPath}");
+            }
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
