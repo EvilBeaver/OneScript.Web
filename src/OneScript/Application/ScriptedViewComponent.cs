d7f49d28b3e133556e0fa4a6f682884e3d257613
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-02-27 18:23:37 +0300

    Начало работ по ViewComponents

---
+++ src/OneScript/Application/ScriptedViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OneScript.WebHost.Application
+{
+    [NonViewComponent]
+    public class ScriptedViewComponent
+    {
+    }
+}
---
+++ src/OneScript/Infrastructure/Implementations/ScriptedViewComponentPartProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace OneScript.WebHost.Infrastructure.Implementations
+{
+    public class ScriptedViewComponentPartProvider : IApplicationFeatureProvider<ViewComponentFeature>
+    {
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, ViewComponentFeature feature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
