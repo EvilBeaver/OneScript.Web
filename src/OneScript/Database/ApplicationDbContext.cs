48085c472b7483d584a1e68d6af3b8d883bf9a51
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-05-22 14:39:41 +0300

    Авторизация по Cookie

--- src/OneScript/Database/ApplicationDbContext.cs
+++ src/OneScript/Database/ApplicationDbContext.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OneScript.WebHost.Identity;
 using OneScript.WebHost.Infrastructure;
 
 namespace OneScript.WebHost.Database
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        private IApplicationRuntime _oscript;
-
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IApplicationRuntime oscript)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IApplicationRuntime oscript):base(options)
         {
-            _oscript = oscript;
+            RuntimeFacility = oscript;
         }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+
+        public IApplicationRuntime RuntimeFacility { get; set; }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- src/OneScriptWeb.Tests/DbModelCreatorTests.cs
+++ src/OneScriptWeb.Tests/DbModelCreatorTests.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Moq;
 using OneScript.WebHost.Database;
+using OneScript.WebHost.Identity;
 using OneScript.WebHost.Infrastructure;
 using Xunit;
 
@@ -18,6 +23,28 @@ namespace OneScriptWeb.Tests
         {
             var opts = new DbContextOptionsBuilder<ApplicationDbContext>();
             var context = new ApplicationDbContext(opts.UseInMemoryDatabase("test1").Options, Mock.Of<IApplicationRuntime>());
+        }
+
+        [Fact]
+        public void CanReadSecurityOptionsFromConfig()
+        {
+            var opts = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var services = new ServiceCollection();
+            services.AddTransient<DbContextOptions<ApplicationDbContext>>((svc)=> opts.UseInMemoryDatabase("usersTest").Options);
+
+            var cfgBuilder = new ConfigurationBuilder();
+            Dictionary<string,string> keys = new Dictionary<string, string>();
+            keys["Security:Password:RequireDigit"] = "false";
+            keys["Security:Password:RequireUppercase"] = "false";
+            cfgBuilder.AddInMemoryCollection(keys);
+
+            services.AddIdentityByConfiguration(cfgBuilder.Build());
+
+            var provider = services.BuildServiceProvider();
+            var result = provider.GetRequiredService<IOptions<IdentityOptions>>().Value;
+            Assert.False(result.Password.RequireDigit);
+            Assert.False(result.Password.RequireUppercase);
         }
     }
 }
