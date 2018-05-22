fa304eb9f1a561d68f0547ae7293a217ad11ec2e
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-05-22 10:57:17 +0300

    Заглушки для функциональности базы данных

--- src/OneScript/Database/ApplicationDbContext.cs
+++ src/OneScript/Database/ApplicationDbContext.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OneScript.WebHost.Infrastructure;
 
 namespace OneScript.WebHost.Database
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        private IApplicationRuntime _oscript;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IApplicationRuntime oscript)
+        {
+            _oscript = oscript;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ src/OneScriptWeb.Tests/DbModelCreatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OneScript.WebHost.Database;
+using OneScript.WebHost.Infrastructure;
+using Xunit;
+
+namespace OneScriptWeb.Tests
+{
+    public class DbModelCreatorTests
+    {
+        [Fact]
+        public void CanCreateDatabaseInMem()
+        {
+            var opts = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var context = new ApplicationDbContext(opts.UseInMemoryDatabase("test1").Options, Mock.Of<IApplicationRuntime>());
+        }
+    }
+}
