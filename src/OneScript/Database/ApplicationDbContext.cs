a57243c61d28e12384e1e0f130b9bfa66f4d75f4
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-05-22 10:27:03 +0300

    Добавлены объекты связи с EF Core

---
+++ src/OneScript/Database/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneScript.WebHost.Database
+{
+    public class ApplicationDbContext
+    {
+    }
+}
