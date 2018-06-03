aff9f7097debf0d3be572ff1ffb425b6c1637a83
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-06-03 20:41:27 +0300

    Строковый ответ компонента

---
+++ src/OneScript/Application/ViewComponentContentResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using ScriptEngine.Machine.Contexts;
+
+namespace OneScript.WebHost.Application
+{
+    [ContextClass("РезультатКомпонентаСодержимое","ViewComponentContentResult")]
+    public class ViewComponentContentResult : AutoContext<ContentActionResult>, IObjectWrapper
+    {
+        private ContentViewComponentResult _realObject;
+
+        [ContextProperty("Содержимое")]
+        public string Content { get; set; }
+
+        public object UnderlyingObject
+        {
+            get
+            {
+                if (_realObject == null)
+                {
+                    _realObject = new ContentViewComponentResult(Content);
+                }
+
+                return _realObject;
+            }
+        }
+
+        [ScriptConstructor]
+        public static ViewComponentContentResult Constructor(string content)
+        {
+            return new ViewComponentContentResult()
+            {
+                Content = content
+            };
+        }
+    }
+}
