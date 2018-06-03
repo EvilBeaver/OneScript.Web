3b81501ee0d22dc585d0d03bd52e51be35252fb1
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-06-03 20:50:30 +0300

    Наброски по ответу от компонента-представления

---
+++ src/OneScript/Application/ViewComponentViewResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using ScriptEngine.Machine.Contexts;
+
+namespace OneScript.WebHost.Application
+{
+    [ContextClass("РезультатКомпонентаПредставление")]
+    public class ViewComponentViewResult : IActionResult
+    {
+        public ViewComponentViewResult()
+        {
+        }
+
+        [ContextProperty("ИмяШаблона")]
+        public string ViewName { get; set; }
+
+        [ContextProperty("ДанныеПредставления")]
+        public ViewDataDictionaryWrapper ViewData { get; set; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            var result = new ViewViewComponentResult();
+            result.ViewData = ViewData.GetDictionary();
+            result.ViewName = ViewName;
+
+            return result.ExecuteAsync((ViewComponentContext)context);
+        }
+    }
+}
