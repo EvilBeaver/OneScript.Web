62958dbefff8f40be53b439a34a07c593b7a8673
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2019-03-26 17:34:54 +0300

    Использован AutoSDO для контроллера и компонента

--- src/OneScript/Application/ScriptedViewComponent.cs
+++ src/OneScript/Application/ScriptedViewComponent.cs
@@ -17,7 +17,7 @@ namespace OneScript.WebHost.Application
 {
     [ContextClass("КомпонентПредставления")]
     [NonViewComponent]
-    public class ScriptedViewComponent : ScriptDrivenObject
+    public class ScriptedViewComponent : AutoScriptDrivenObject<ScriptedViewComponent>
     {
         public const string InvokeMethodNameRu = "ОбработкаВызова";
         public const string InvokeMethodNameEn = "CallProcessing";
@@ -28,14 +28,6 @@ namespace OneScript.WebHost.Application
         private SessionImpl _session;
         private ViewComponentContext _ctx;
         private ViewDataDictionaryWrapper _osViewData;
-
-        private const int THISOBJ_VARIABLE_INDEX = 0;
-        private const string THISOBJ_EN = "ThisObject";
-        private const string THISOBJ_RU = "ЭтотОбъект";
-        private const int PRIVATE_PROPS_OFFSET = 1;
-
-        private static ContextPropertyMapper<ScriptedViewComponent> _ownProperties = new ContextPropertyMapper<ScriptedViewComponent>();
-        private static ContextMethodsMapper<ScriptedViewComponent> _ownMethods = new ContextMethodsMapper<ScriptedViewComponent>();
 
         public ScriptedViewComponent(LoadedModule module, string dynamicTypeName) : base(module, true)
         {
@@ -320,122 +312,6 @@ namespace OneScript.WebHost.Application
         private ViewComponentViewResult DefaultViewResult()
         {
             return new ViewComponentViewResult() { ViewData = ViewData };
-        }
-
-        #region SDO Methods
-
-        protected override string GetOwnPropName(int index)
-        {
-            if (index == THISOBJ_VARIABLE_INDEX)
-                return THISOBJ_RU;
-
-            return _ownProperties.GetProperty(index - PRIVATE_PROPS_OFFSET).Name;
-        }
-
-        protected override int GetOwnVariableCount()
-        {
-            return _ownProperties.Count + PRIVATE_PROPS_OFFSET;
-        }
-
-        protected override int GetOwnMethodCount()
-        {
-            return _ownMethods.Count;
-        }
-
-        protected override void UpdateState()
-        {
-        }
-
-        protected override int FindOwnProperty(string name)
-        {
-            if (string.Compare(name, THISOBJ_RU, StringComparison.OrdinalIgnoreCase) == 0
-                || string.Compare(name, THISOBJ_EN, StringComparison.OrdinalIgnoreCase) == 0)
-            {
-                return THISOBJ_VARIABLE_INDEX;
-            }
-
-            return _ownProperties.FindProperty(name) + PRIVATE_PROPS_OFFSET;
-        }
-
-        public override int FindProperty(string name)
-        {
-            return base.FindProperty(name);
-        }
-
-        protected override bool IsOwnPropReadable(int index)
-        {
-            if (index == THISOBJ_VARIABLE_INDEX)
-                return true;
-
-            return _ownProperties.GetProperty(index - PRIVATE_PROPS_OFFSET).CanRead;
-        }
-
-        protected override bool IsOwnPropWritable(int index)
-        {
-            if (index == THISOBJ_VARIABLE_INDEX)
-                return false;
-
-            return _ownProperties.GetProperty(index - PRIVATE_PROPS_OFFSET).CanWrite;
-        }
-
-        protected override IValue GetOwnPropValue(int index)
-        {
-            if (index == THISOBJ_VARIABLE_INDEX)
-                return this;
-
-            return _ownProperties.GetProperty(index - PRIVATE_PROPS_OFFSET).Getter(this);
-        }
-
-        protected override void SetOwnPropValue(int index, IValue val)
-        {
-            _ownProperties.GetProperty(index - PRIVATE_PROPS_OFFSET).Setter(this, val);
-        }
-
-        protected override int FindOwnMethod(string name)
-        {
-            try
-            {
-                int idx = _ownMethods.FindMethod(name);
-                return idx;
-            }
-            catch (RuntimeException)
-            {
-                return -1;
-            }
-        }
-
-        protected override MethodInfo GetOwnMethod(int index)
-        {
-            return _ownMethods.GetMethodInfo(index);
-        }
-
-        protected override IValue CallOwnFunction(int index, IValue[] arguments)
-        {
-            return _ownMethods.GetMethod(index)(this, arguments);
-        }
-
-        protected override void CallOwnProcedure(int index, IValue[] arguments)
-        {
-            _ownMethods.GetMethod(index)(this, arguments);
-        }
-
-        #endregion
-
-        public static ModuleImage CompileModule(CompilerService compiler, ICodeSource src)
-        {
-            compiler.DefineVariable(THISOBJ_RU, THISOBJ_EN, SymbolType.ContextProperty);
-            for (int i = 0; i < _ownProperties.Count; i++)
-            {
-                var currentProp = _ownProperties.GetProperty(i);
-                compiler.DefineVariable(currentProp.Name, currentProp.Alias, SymbolType.ContextProperty);
-            }
-
-            for (int i = 0; i < _ownMethods.Count; i++)
-            {
-                compiler.DefineMethod(_ownMethods.GetMethodInfo(i));
-            }
-
-            return compiler.Compile(src);
         }
 
     }
