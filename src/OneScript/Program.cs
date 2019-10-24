da8582547312155c7169fafd47784dcd102eb77f
Artem Kuznetsov <ArKuznetsov@gmail.com>
2019-10-24 19:21:00 +0300

    refactor: Изменено имя параметра командной строки для указания каталога приложения appPath -> ContentRoot; Рефакторинг кода получения параметров командной строки; feat: Добавлена возможность указания параметра командной строки --RunAsService, для запуска приложения в качестве сервиса; feat: Добавлена возможность указания параметра командной строки --urls, для указания адреса и порта приложения Oscript.Web.

--- src/OneScript/Program.cs
+++ src/OneScript/Program.cs
@@ -20,29 +20,32 @@ namespace OneScript.WebHost
             if (location != null)
                 config.AddJsonFile(Path.Combine(location, "appsettings.json"), optional: true);
 
-            if (args != null)
-            {
-                config.AddCommandLine(args);
-            }
+            var contentRoot = GetContentRoot(args);
 
-            var configInstance = config.Build();
-
-            string appPath = configInstance.GetValue<string>("AppPath", "");
-
-            if (appPath == "")
-                appPath = Directory.GetCurrentDirectory();
-
-            config.SetBasePath(appPath);
+            config.SetBasePath(contentRoot);
             config.AddJsonFile("appsettings.json", optional: true);
             config.AddEnvironmentVariables("OSWEB_");
             config.AddEnvironmentVariables("ASPNETCORE_");
 
             if (args != null)
             {
-                config.AddCommandLine(args);
+                var argMappings = new Dictionary<string, string>()
+                {
+                    { "--ContentRoot", "Hosting:ContentRoot" },
+                    { "--RunAsService", "Hosting:RunAsService" },
+                    { "--Urls", "Hosting:Urls" }
+                };
+                config.AddCommandLine(args, argMappings);
+                argMappings = new Dictionary<string, string>()
+                {
+                    { "--ContentRoot", "ContentRoot" },
+                    { "--RunAsService", "RunAsService" },
+                    { "--Urls", "Urls" }
+                };
+                config.AddCommandLine(args, argMappings);
             }
 
-            configInstance = config.Build();
+            var configInstance = config.Build();
             var builder = new WebHostBuilder();
             var options = ConfigureHostingMode(builder, configInstance);
 
@@ -75,11 +78,6 @@ namespace OneScript.WebHost
             var options = new HostingOptions();
             config.GetSection("Hosting").Bind(options);
 
-            string appPath = config.GetValue<string>("AppPath", "");
-
-            if (appPath != "")
-                options.ContentRoot = appPath;
-
             if (options.ContentRoot == null)
             {
                 options.ContentRoot = options.RunAsService
@@ -103,6 +101,26 @@ namespace OneScript.WebHost
             public bool RunAsService { get; set; }
             public string ContentRoot { get; set; }
             public string WebRoot { get; set; }
+
+        }
+
+        private static string GetContentRoot(string[] args)
+        {
+            var config = new ConfigurationBuilder();
+
+            if (args != null)
+            {
+                config.AddCommandLine(args);
+            }
+
+            var configInstance = config.Build();
+
+            string contentRoot = configInstance.GetValue<string>("ContentRoot", "");
+
+            if (contentRoot == "")
+                contentRoot = Directory.GetCurrentDirectory();
+
+            return contentRoot;
 
         }
     }
