5f285142f6c41df55e36083fb1028b6b754e2b64
boris stoyanov <info@stoyanov-boris.ru>
2019-12-13 18:48:15 +0500

    заготовка middleware

---
+++ src/OneScript/Application/MiddwareInstance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ScriptEngine.Machine.Contexts;
+
+namespace OneScript.WebHost.Application
+{
+    /// <summary>
+    /// Экземпляр посредника. Представлен модулем посредника в подпапке middleware проекта
+    /// </summary>
+    [ContextClass("Посредник")]
+    public class MiddlewareInstance
+    {
+        private readonly RequestDelegate _next;
+        private HttpContext _context;
+
+        /// <summary>
+        /// Конструктор посредника
+        /// </summary>
+        /// <param name="next"></param>
+        public MiddlewareInstance(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            this._context = context;
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ContextMethod("Следующий")]
+        public void InvokeNext()
+        {
+            _next.Invoke(this._context);
+        }
+    }
+}
